43c61f57cb524802a5c1fe7ee4977902d079ba19
David Aerne <meodai@gmail.com>
2016-11-11 21:06:57 +0100

    makes js work with webpack

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var imagemin = require('gulp-imagemin');
 var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var webpack = require('webpack-stream');
+
 
 var folders = {
   src: 'src/',
@@ -19,9 +21,23 @@ var files = {
   sass: folders.src + 'css/**/*.scss',
   fonts: folders.src + 'font/**/*.{ttf,woff,eof,eot,css,svg,otf}',
   images: folders.src + 'image/*',
-  pug: folders.src + '*.pug'
+  pug: folders.src + '*.pug',
+  js: folders.src + '/**/*.js'
 };
 
+gulp.task('js', function() {
+  return gulp.src(files.js)
+    .pipe(webpack({
+      output: {
+        filename: 'main.js'
+      },
+      resolve: {
+          // Makes sure the compiler looks for modules in /src and node_modules
+          modulesDirectories: [folders.build + '/js', 'node_modules']
+        }
+    }))
+    .pipe(gulp.dest(folders.build + 'js/'));
+});
 
 gulp.task('clean', function(){
   return gulp.src(folders.build + '/*', {read: false}).pipe(clean());
@@ -63,8 +79,9 @@ gulp.task('serve', function() {
   gulp.watch(files.pug, gulp.series('html', reload));
   gulp.watch(files.images, gulp.series('images', reload));
   gulp.watch(files.sass, gulp.series('sass', reload));
+  gulp.watch(files.js, gulp.series('js', reload));
 });
 
-gulp.task('all', gulp.parallel('copy:fonts', 'images', 'sass', 'html'));
+gulp.task('all', gulp.parallel('copy:fonts', 'images', 'sass', 'html', 'js'));
 
 gulp.task('default', gulp.series('clean', 'all', 'serve'));
--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,24 @@
 'use strict';
 
-function menuFunction()
+window.menuFunction = function()
 {
     document.getElementById("nav-glossary").classList.add("menu-active");
     document.getElementById("nav-glossary").classList.remove("glossary-active");
     document.getElementById("body").classList.add("overflow");
 };
-function menulistFunction()
+window.menulistFunction = function()
 {
     document.getElementById("nav-glossary").classList.remove("menu-active");
     document.getElementById("body").classList.remove("overflow");
 
 };
-function glossaryFunction()
+window.glossaryFunction = function()
 {
     document.getElementById("nav-glossary").classList.add("glossary-active");
     document.getElementById("nav-glossary").classList.remove("menu-active");
     document.getElementById("body").classList.add("overflow");
 };
-function dismissFunction()
+window.dismissFunction = function()
 {
     document.getElementById("nav-glossary").classList.remove("menu-active", "glossary-active");
     document.getElementById("body").classList.remove("overflow");
